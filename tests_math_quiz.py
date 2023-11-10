ebf7ee24019ebe8fdb5068a93e9f1fa06130edab
ULRA92 <150431330+ULRA92@users.noreply.github.com>
2023-11-10 13:05:16 +0100

    Update tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -1,29 +1,36 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import test_generate_Integer, random-arithmatic_Operator, calculate_numbers
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_Integer(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+        min = 1
+        max = 10
+        for _ in range(1000):  
+            # Test a large number of random values
+            rand_number = generate_Integer(min, max)
+            self.assertTrue(min <= rand_number <= max)
 
-    def test_function_B(self):
-        # TODO
+    def test_random-arithmatic_Operator(self):
+    """ test if the generated operated is one of the defined arithmetic operators """
+        arithmetic_Operators = {'+', '-', '*'}
+            self.assertTrue(arithmetic_Operator)
         pass
 
-    def test_function_C(self):
+
+    def test_calculate_numbers(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (-2,3, '-', -(-2)-3, -1),
+                (9,1, '*', '9*1', 9)
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
+                operation, answer = calculate_numbers(n1,n2, operator)
+                self.assertEqual(problem, expected_problem)
+                self.assertEqual(answer, expected_answer)
                 pass
 
 if __name__ == "__main__":
