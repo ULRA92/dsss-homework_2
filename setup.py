20f3ba40517a6e44f004ccd3ac625119e6d9cb99
ULRA92 <150431330+ULRA92@users.noreply.github.com>
2023-11-12 22:33:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='math-quiz-game',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        
+        # Add any dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'math-quiz=math_quiz.math_quiz:math_quiz',
+        ],
+    },
+    
+)
